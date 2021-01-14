d6286e3038eca2829fb6d46bd5b0ff5f308f8ac0
Ryan Haire <rwhaire@hotmail.com>
2021-01-14 14:48:41 -0500

    added production

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,13 @@ connectDB()
 
 const app = express()
 
+if(process.env.NODE_ENV == 'production') {
+    app.use(express.static('frontend/build'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    })
+}
+
 app.get('/', (req, res) => {
     res.send('API is running')
 })
