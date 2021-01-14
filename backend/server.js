5d8b22325aac01e366bce943800ff9f5abbe5cc9
Ryan Haire <rwhaire@hotmail.com>
2021-01-14 15:22:50 -0500

    fixed up backend problems with serving react

--- backend/server.js
+++ backend/server.js
@@ -17,14 +17,20 @@ if(process.env.NODE_ENV == 'production') {
     app.use(express.static(path.join(__dirname, 'frontend/build')))
 }
 
-app.get('/', (req, res) => {
-    res.send('API is running')
-})
+// app.get('/', (req, res) => {
+//     res.send('API is running')
+// })
 
 app.use('/api/products', productRoutes)
 
 app.use(notFound)
 app.use(errorHandler)
+
+if(process.env.NODE_ENV == 'production') {
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname+'/frontend/build/index.html'))
+    })
+}
 
 const PORT = process.env.PORT || 5000
 
