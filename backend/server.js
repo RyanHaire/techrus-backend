e889c9db8efa290b94e23d0d47a5f68e77a6de3e
Ryan Haire <rwhaire@hotmail.com>
2021-01-14 17:11:23 -0500

    commit message

--- backend/server.js
+++ backend/server.js
@@ -21,10 +21,7 @@ const app = express()
 
 app.use(cors('*'))
 
-if(process.env.NODE_ENV == 'production') {
-    app.use(express.static(path.join(__dirname, 'frontend/build')))
-    console.log('hello world')
-}
+
 
 // app.get('/', (req, res) => {
 //     res.send('API is running')
@@ -35,12 +32,17 @@ app.use('/api/products', productRoutes)
 app.use(notFound)
 app.use(errorHandler)
 
-if(process.env.NODE_ENV == 'production') {
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname+'/frontend/build/index.html'))
-    })
-}
 
 const PORT = process.env.PORT || 5000
+
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(__dirname + "/frontend/build"));
+
+    const path = require("path");
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+    });
+}
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
