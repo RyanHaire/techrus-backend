be42b1b49150805564f46db3e004e29d1907f543
Ryan Haire <rwhaire@hotmail.com>
2021-01-14 17:13:12 -0500

    commit message

--- backend/server.js
+++ backend/server.js
@@ -39,7 +39,6 @@ const PORT = process.env.PORT || 5000
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(__dirname + "/frontend/build"));
 
-    const path = require("path");
     app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
     });
