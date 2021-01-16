0dd8bd93d7e106a0081d0836daf0a3fe4d868359
Ryan Haire <rwhaire@hotmail.com>
2021-01-16 08:28:20 -0500

    added user pw to env

--- data/user.js
+++ data/user.js
@@ -1,21 +1,24 @@
 import bcyrpt from 'bcryptjs'
+import dotend from 'dotenv'
+
+dotenv.config()
 
 const users = [
     {
         name: 'Admin User',
         email: 'admin@example.com',
-        password: bcyrpt.hashSync('123456', 10),
+        password: bcyrpt.hashSync(process.env.USER_PW, 10),
         isAdmin: true,
     },
     {
         name: 'John Doe',
         email: 'john@example.com',
-        password: bcyrpt.hashSync('123456', 10),
+        password: bcyrpt.hashSync(process.env.USER_PW, 10),
     },
     {
         name: 'Jane Doe',
         email: 'jane@example.com',
-        password: bcyrpt.hashSync('123456', 10),
+        password: bcyrpt.hashSync(process.env.USER_PW, 10),
     },
 ]
 
